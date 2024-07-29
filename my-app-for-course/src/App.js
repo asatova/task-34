12d702aeae74f4410aba9bb5ebf17c1c8183b2be
zarnigor <zarnigorasatova52@gmail.com>
2024-07-29 11:19:30 +0500

    feature

--- my-app-for-course/src/App.js
+++ my-app-for-course/src/App.js
@@ -8,42 +8,42 @@ class App extends Component {
             <div className="main-flex">
                 <Product
                     key="1"
-                    img src="./images/3.png"
+                    img ="./images/3.png"
                     name="Orange Juice"
                     info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae"
                     price="$ 7.9"
                 />
                 <Product
                     key="2"
-                    img src="./images/3.png"
+                    img="./images/3.png"
                     name="Banana Juice"
                     info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae"
                     price="$ 2.9"
                 />
                 <Product
                     key="3"
-                    img src="./images/3.png"
+                    img="./images/3.png"
                     name="Avocado Juice"
                     info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae"
                     price="$ 4.8"
                 />
                 <Product
                     key="4"
-                    img src="./images/3.png"
+                    img ="./images/3.png"
                     name="Lemon Juice"
                     info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae"
                     price="$ 5.9"
                 />
                 <Product
                     key="5"
-                    img src="./images/3.png"
+                    img ="./images/3.png"
                     name="Banana Juice"
                     info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae"
                     price="$ 2.9"
                 />
                 <Product
                     key="6"
-                    img src="./images/3.png"
+                    img ="./images/3.png"
                     name="Avocado Juice"
                     info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae"
                     price="$ 4.8"
